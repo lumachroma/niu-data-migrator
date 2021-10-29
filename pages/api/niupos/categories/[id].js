f557312db6902541d6d89b78c4ea8b67067be96b
Nazrul Hisham <nazrul.daud@sapuraenergy.com>
2021-10-30 02:43:57 +0800

    Implement endpoint to get category by id

---
+++ pages/api/niupos/categories/[id].js
@@ -0,0 +1,15 @@
+import nextConnect from 'next-connect'
+import { all } from '../../../../middleware'
+import fetcher from '../../../../utils/fetcher'
+
+const handler = nextConnect()
+
+handler.use(all)
+
+handler.get(async (req, res) => {
+  const { query: { id } } = req
+  const data = await fetcher(`https://buraq-api-v1.niulabs.co/api/services/app/ProductCategory/GetCategory?categoryId=${id}`)
+  res.status(200).json(data)
+})
+
+export default handler
