8b2770369742c53715a5833ab01bcfa2e87f5b17
Nazrul Hisham <nazrul.daud@sapuraenergy.com>
2021-10-31 13:26:00 +0800

    Add ability to remove/include _id from products & supplier endpoints

--- pages/api/barupos/products.js
+++ pages/api/barupos/products.js
@@ -7,26 +7,31 @@ const handler = nextConnect()
 handler.use(all)
 
 handler.get(async (req, res) => {
+    const { query: { includeId } } = req
+    let projection = {
+        _id: 0,
+        level: 1,
+        siteID: 1,
+        barcodes: 1,
+        overrideAllowed: 1,
+        active: 1,
+        isDeleted: 1,
+        name: 1,
+        printName: 1,
+        category: 1,
+        sku: 1,
+        description: 1,
+        price: 1,
+        productType: 1,
+        isReturnable: 1,
+        tax: 1,
+        brand: 1,
+    }
+    if (includeId && includeId === "true") {
+        projection._id = 1
+    }
     try {
-        let allProducts = await Product.find({}, {
-            _id: 0,
-            level: 1,
-            siteID: 1,
-            barcodes: 1,
-            overrideAllowed: 1,
-            active: 1,
-            isDeleted: 1,
-            name: 1,
-            printName: 1,
-            category: 1,
-            sku: 1,
-            description: 1,
-            price: 1,
-            productType: 1,
-            isReturnable: 1,
-            tax: 1,
-            brand: 1,
-        }).lean()
+        let allProducts = await Product.find({}, projection).lean()
         res.status(200).json({ result: allProducts, success: true })
     } catch (error) {
         res.status(500).json({ result: {}, success: false, error })
--- pages/api/barupos/suppliers.js
+++ pages/api/barupos/suppliers.js
@@ -7,16 +7,21 @@ const handler = nextConnect()
 handler.use(all)
 
 handler.get(async (req, res) => {
+    const { query: { includeId } } = req
+    let projection = {
+        _id: 0,
+        name: 1,
+        address: 1,
+        phoneNumber: 1,
+        companyNumber: 1,
+        contact: 1,
+        contracts: 1,
+    }
+    if (includeId && includeId === "true") {
+        projection._id = 1
+    }
     try {
-        let allSuppliers = await Supplier.find({}, {
-            _id: 0,
-            name: 1,
-            address: 1,
-            phoneNumber: 1,
-            companyNumber: 1,
-            contact: 1,
-            contracts: 1,
-        }).lean()
+        let allSuppliers = await Supplier.find({}, projection).lean()
         res.status(200).json({ result: allSuppliers, success: true })
     } catch (error) {
         res.status(500).json({ result: {}, success: false, error })
